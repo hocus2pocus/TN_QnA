b85a4160ea80231198dc93a989790d47f100e90e
Eugene I <eugene_i@outlook.com>
2019-08-09 23:24:17 +0300

    AnswersController, new and create actions plus tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -15,4 +15,45 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    it 'assigns a new Answer to @answer' do
+      get :new, params: { question_id: question }
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      get :new, params: { question_id: question }
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    it "proves that new Answer is owned by @question" do
+      post :create, params: { answer: attributes_for(:answer), question_id: question }
+      expect(assigns(:answer).question).to eq(question)
+    end
+
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
