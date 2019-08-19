b68093ebcfa2ed9876c731ba21a83e25d933e452
Eugene I <eugene_i@outlook.com>
2019-08-20 01:37:40 +0300

    History complete: user is able to sign up

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -5,7 +5,6 @@ feature 'User can sign out', %q{
   As an authenticated user
   I'd like to be able to sign out
 } do
-
   scenario 'Authenticated user tries to sign out' do
     user = create(:user)
     sign_in(user)
